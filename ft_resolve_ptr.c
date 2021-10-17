b5830b751c010009f0ff65454ae4602fceb7ccdf
Igor Davoli <igordavoli@ioasys.com.br>
2021-10-16 21:46:52 -0300

    test poiter

--- ft_resolve_ptr.c
+++ ft_resolve_ptr.c
@@ -6,7 +6,7 @@
 /*   By: idavoli- <idavoli-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 03:30:00 by idavoli-          #+#    #+#             */
-/*   Updated: 2021/10/14 23:46:01 by idavoli-         ###   ########.fr       */
+/*   Updated: 2021/10/16 21:46:18 by idavoli-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int	ft_resolve_ptr(unsigned long long ptr)
 {
 	int				i;
 
-	if (!ptr)
-	{
-		ft_putstr_fd("(nil)", 1);
-		return (5);
-	}
+	// if (!ptr)
+	// {
+	// 	ft_putstr_fd("(nil)", 1);
+	// 	return (5);
+	// }
 	ft_putstr_fd("0x", 1);
 	ft_putnbr_hex(ptr, 0);
 	ptr /= 16;
